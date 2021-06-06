73ec7ad217ca9a71958bcc7c3d0279ceab9c0903
Joe Nave <navej2012@gmail.com>
2021-06-05 20:55:58 -0700

    update test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 docker build .
+if [ $? -eq 0 ]
+then
+  echo "All tests passed! Congratulations the project is done!"
+  exit 0
+else
+  echo "Tests did not pass. See the log output on what to fix." >&2
+  exit 1
+fi
